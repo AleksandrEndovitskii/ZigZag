b82e91ea801264c7a55c53188f4e2a4cf1724515
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-21 17:09:23 +0300

    added ClickHandler

---
+++ Assets/Scripts/Components/ClickHandlerComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components
+{
+    [RequireComponent(typeof(Button))]
+    public class ClickHandlerComponent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            this.gameObject.GetComponent<Button>().onClick.AddListener(ButtonOnClick);
+        }
+        private void OnDestroy()
+        {
+            this.gameObject.GetComponent<Button>().onClick.RemoveListener(ButtonOnClick);
+        }
+
+        private void ButtonOnClick()
+        {
+            Debug.Log("Click was handled.");
+        }
+    }
+}
