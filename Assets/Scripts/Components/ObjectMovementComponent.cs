b07e4b04ebde6b2ae47d3054b15618ed55abd875
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-24 07:35:41 +0300

    added ObjectMovementComponent movement direction from MovementDirectionService

--- Assets/Scripts/Components/ObjectMovementComponent.cs
+++ Assets/Scripts/Components/ObjectMovementComponent.cs
@@ -9,7 +9,8 @@ namespace Components
         {
             // Move the object forward along its z axis 1 unit/second.
             this.gameObject.transform.Translate(
-                Vector3.up * GameManager.Instance.MovementSpeedService.CurrentMovementSpeed * Time.deltaTime,
+                GameManager.Instance.MovementDirectionService.CurrentMovementDirection *
+                GameManager.Instance.MovementSpeedService.CurrentMovementSpeed * Time.deltaTime,
                 Space.World);
         }
     }
