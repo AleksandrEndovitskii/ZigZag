2b15698ec025cc2718e24e56c98c29c69a5ee4b3
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-24 06:49:02 +0300

    added DirectionalLight instantiating

--- Assets/Scripts/Managers/GameObjectsManager.cs
+++ Assets/Scripts/Managers/GameObjectsManager.cs
@@ -6,12 +6,19 @@ namespace Managers
     public class GameObjectsManager : MonoBehaviour
     {
         [SerializeField]
+        private Light _directionalLightPrefab;
+
+        [SerializeField]
         private FieldView _fieldViewPrefab;
+
+        private Light _directionalLightInstance;
 
         private FieldView _fieldViewInstance;
 
         public void Initialize()
         {
+            _directionalLightInstance = Instantiate(_directionalLightPrefab);
+
             _fieldViewInstance = Instantiate(_fieldViewPrefab);
         }
     }
