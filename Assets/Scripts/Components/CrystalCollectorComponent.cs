09e7990c21b238a69e0767373054f4effe8b9b02
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-24 06:19:16 +0300

    added CrystalCollectorComponent

---
+++ Assets/Scripts/Components/CrystalCollectorComponent.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using Managers;
+using UnityEngine;
+
+namespace Components
+{
+    public class CrystalCollectorComponent : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            // Если шарик наезжает на кристалл, то кристалл исчезает с тайла и считается, что игрок подобрал этот кристалл.
+            var crystalView = collision.gameObject.GetComponent<CrystalView>();
+            if (crystalView != null)
+            {
+                Destroy(crystalView.gameObject);
+
+                GameManager.Instance.CollectedCrystalsCountingService.CollectedCrystalsCount++;
+            }
+        }
+    }
+}
