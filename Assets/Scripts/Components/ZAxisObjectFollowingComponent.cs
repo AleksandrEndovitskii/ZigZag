ba1d1ca276e231c413c0b7f94e0387c870266c7d
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-25 02:43:04 +0300

    fixed camera positioning

--- Assets/Scripts/Components/ZAxisObjectFollowingComponent.cs
+++ Assets/Scripts/Components/ZAxisObjectFollowingComponent.cs
@@ -5,6 +5,8 @@ namespace Components
 {
     public class ZAxisObjectFollowingComponent : MonoBehaviour
     {
+        private const float HEIGHT_CORRECTION = 3f;
+
         // Камера при этом двигается за шариком таким образом, чтобы он всегда находился в центре экрана по вертикали.
         private void Update()
         {
@@ -17,8 +19,8 @@ namespace Components
 
             this.gameObject.transform.position = new Vector3(
                 this.gameObject.transform.position.x,
-                this.gameObject.transform.position.y,
-                targetObject.transform.position.z);
+                targetObject.transform.position.x * 0.5f + targetObject.transform.position.z * 0.5f + HEIGHT_CORRECTION,
+                this.gameObject.transform.position.z);
         }
     }
 }
