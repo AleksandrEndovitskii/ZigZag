596c52d0ca2f9aefb30c3c137753460732026858
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-24 05:41:03 +0300

    added starting movement on changing game status

--- Assets/Scripts/Components/MovementComponent.cs
+++ Assets/Scripts/Components/MovementComponent.cs
@@ -1,9 +1,28 @@
-﻿using UnityEngine;
+﻿using Managers;
+using Services;
+using UnityEngine;
 
 namespace Components
 {
     public class MovementComponent : MonoBehaviour
     {
+        private void Awake()
+        {
+            GameManager.Instance.GameStateService.CurrentGameStateChanged += GameStateServiceCurrentGameStateChanged;
+        }
 
+        private void GameStateServiceCurrentGameStateChanged(GameState gameState)
+        {
+            if (gameState == GameState.InProgress)
+            {
+                StartMovement();
+            }
+        }
+
+        // При начале игры шарик не двигается до тех пор, пока пользователь не кликнет в любой части экрана.
+        private void StartMovement()
+        {
+
+        }
     }
 }
