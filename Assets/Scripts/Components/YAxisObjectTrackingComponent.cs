92b7964cba50199b6c642c1bb0020b3af553651c
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-25 01:55:22 +0300

    added YAxisObjectTrackingComponent

---
+++ Assets/Scripts/Components/YAxisObjectTrackingComponent.cs
@@ -0,0 +1,18 @@
+﻿using Managers;
+using Services;
+using UnityEngine;
+
+namespace Components
+{
+    public class YAxisObjectTrackingComponent : MonoBehaviour
+    {
+        // Если шарик выходит за рамки тайла в пустое пространство (туда, где нет тайла), то  шарик падает за поле
+        private void Update()
+        {
+            if (this.gameObject.transform.position.y < 0)
+            {
+                GameManager.Instance.GameStateService.CurrentGameState = GameState.Finished;
+            }
+        }
+    }
+}
