263869c949b9b1f78ad9fec1d4de2f1f87eda69b
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-24 07:27:36 +0300

    added ObjectMovementComponent

---
+++ Assets/Scripts/Components/ObjectMovementComponent.cs
@@ -0,0 +1,16 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Components
+{
+    public class ObjectMovementComponent : MonoBehaviour
+    {
+        private void Update()
+        {
+            // Move the object forward along its z axis 1 unit/second.
+            this.gameObject.transform.Translate(
+                Vector3.up * GameManager.Instance.MovementSpeedService.CurrentMovementSpeed * Time.deltaTime,
+                Space.World);
+        }
+    }
+}
