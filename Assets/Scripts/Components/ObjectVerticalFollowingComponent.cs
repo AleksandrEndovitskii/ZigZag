9ea4195de417bcd57716b054ceacbc9a2b61f231
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-25 01:17:59 +0300

    added ObjectVerticalFollowingComponent

---
+++ Assets/Scripts/Components/ObjectVerticalFollowingComponent.cs
@@ -0,0 +1,19 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Components
+{
+    public class ObjectVerticalFollowingComponent : MonoBehaviour
+    {
+        // Камера при этом двигается за шариком таким образом, чтобы он всегда находился в центре экрана по вертикали.
+        private void Update()
+        {
+            var targetObject = GameManager.Instance.GameObjectsManager.BallViewInstance;
+
+            this.gameObject.transform.position = new Vector3(
+                this.gameObject.transform.position.x,
+                this.gameObject.transform.position.y,
+                targetObject.transform.position.z);
+        }
+    }
+}
