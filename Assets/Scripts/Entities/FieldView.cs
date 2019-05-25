cad85927e9e6da1a2349159357a2949a8f68c3c2
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-26 01:08:42 +0300

    moved functional for spawning Cells to CellsVisibilityChangedHandlerService

--- Assets/Scripts/Entities/FieldView.cs
+++ Assets/Scripts/Entities/FieldView.cs
@@ -33,21 +33,7 @@ namespace Entities
                 }
             }
 
-            _lastSpawnedCellViewInstance.gameObject.GetComponent<VisibilityDetectionComponent>().IsVisibleChanged += LastSpawnedCellViewInstanceIsVisibleChanged;
-        }
-
-        private void LastSpawnedCellViewInstanceIsVisibleChanged(MonoBehaviour monoBehaviour)
-        {
-            if (!monoBehaviour.gameObject.GetComponent<Renderer>().isVisible)
-            {
-                return;
-            }
-
-            _lastSpawnedCellViewInstance.gameObject.GetComponent<VisibilityDetectionComponent>().IsVisibleChanged -= LastSpawnedCellViewInstanceIsVisibleChanged;
-
-            // Поле генерируется случайным образом бесконечно, таким образом, чтобы по нему мог пройти шарик (не должно быть непроходимых участков).
-            var cellView = GameManager.Instance.RandomFiledGenerationService.AddCellToCell(monoBehaviour.GetComponent<CellView>());
-            cellView.gameObject.GetComponent<VisibilityDetectionComponent>().IsVisibleChanged += LastSpawnedCellViewInstanceIsVisibleChanged;
+            _lastSpawnedCellViewInstance.gameObject.AddComponent<CellSpawnerComponent>();
         }
     }
 }
--- Assets/Scripts/Services/CellsVisibilityChangedHandlerService.cs
+++ Assets/Scripts/Services/CellsVisibilityChangedHandlerService.cs
@@ -1,4 +1,8 @@
-﻿using Utils;
+﻿using Components;
+using Entities;
+using Managers;
+using UnityEngine;
+using Utils;
 
 namespace Services
 {
@@ -12,6 +16,25 @@ namespace Services
         public void UnInitialize()
         {
 
+        }
+
+        public void WatchForBecameVisible(CellView cellView)
+        {
+            cellView.gameObject.GetComponent<VisibilityDetectionComponent>().IsVisibleChanged += LastSpawnedCellViewInstanceIsVisibleChanged;
+        }
+
+        private void LastSpawnedCellViewInstanceIsVisibleChanged(MonoBehaviour monoBehaviour)
+        {
+            if (!monoBehaviour.gameObject.GetComponent<Renderer>().isVisible)
+            {
+                return;
+            }
+
+            monoBehaviour.gameObject.GetComponent<VisibilityDetectionComponent>().IsVisibleChanged -= LastSpawnedCellViewInstanceIsVisibleChanged;
+
+            // Поле генерируется случайным образом бесконечно, таким образом, чтобы по нему мог пройти шарик (не должно быть непроходимых участков).
+            var cellView = GameManager.Instance.RandomFiledGenerationService.AddCellToCell(monoBehaviour.GetComponent<CellView>());
+            cellView.gameObject.GetComponent<VisibilityDetectionComponent>().IsVisibleChanged += LastSpawnedCellViewInstanceIsVisibleChanged;
         }
     }
 }
