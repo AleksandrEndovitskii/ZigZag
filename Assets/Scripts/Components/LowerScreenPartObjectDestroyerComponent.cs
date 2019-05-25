75bab1220f65fb653834f8afab4554347e4505e5
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-26 02:51:53 +0300

    added animation of falling Cells

--- Assets/Scripts/Components/LowerScreenPartObjectDestroyerComponent.cs
+++ Assets/Scripts/Components/LowerScreenPartObjectDestroyerComponent.cs
@@ -1,17 +1,26 @@
-﻿using UnityEngine;
+﻿using Managers;
+using UnityEngine;
 
 namespace Components
 {
     public class LowerScreenPartObjectDestroyerComponent : MonoBehaviour
     {
-        private const int SCREEN_PART_DESTROY_TRIGGER = 4; // 1/4 of screen will trigger destruction
+        private readonly Vector3 ANIMATION_DIRECTION = Vector3.down;
+        private const float ANIMATION_SPEED = 1;
 
         private void Update()
         {
-            var screenPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
-            if (screenPosition.y < Screen.height / SCREEN_PART_DESTROY_TRIGGER)
+            // добавить анимации пропадания тайлов, находящихся позади шарика (те, которые уже были пройдены)
+            if (this.gameObject.transform.position.x < GameManager.Instance.GameObjectsManager.BallViewInstance.gameObject.transform.position.x - 1 &&
+                this.gameObject.transform.position.z < GameManager.Instance.GameObjectsManager.BallViewInstance.gameObject.transform.position.z - 1)
             {
-                Destroy(this.gameObject);
+                this.gameObject.transform.Translate(ANIMATION_DIRECTION * ANIMATION_SPEED * Time.deltaTime,
+                    Space.World);
+
+                if (this.gameObject.GetComponent<InvisibleObjectDestroyerComponent>() == null)
+                {
+                    this.gameObject.AddComponent<InvisibleObjectDestroyerComponent>();
+                }
             }
         }
     }
