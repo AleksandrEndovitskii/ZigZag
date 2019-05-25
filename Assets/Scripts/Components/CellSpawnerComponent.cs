c510acbc35f519f76117fae77c7e5099420a1d44
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-26 01:04:57 +0300

    added CellSpawnerComponent

---
+++ Assets/Scripts/Components/CellSpawnerComponent.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using Managers;
+using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(Renderer))]
+    [RequireComponent(typeof(CellView))]
+    public class CellSpawnerComponent : MonoBehaviour
+    {
+        private void OnBecameVisible()
+        {
+            Destroy(this);
+
+            // Поле генерируется случайным образом бесконечно, таким образом, чтобы по нему мог пройти шарик (не должно быть непроходимых участков).
+            var cellView = GameManager.Instance.RandomFiledGenerationService.AddCellToCell(this.gameObject.GetComponent<CellView>());
+
+            cellView.gameObject.AddComponent<CellSpawnerComponent>();
+        }
+    }
+}
