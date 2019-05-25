00d0f8d78fab93568b922723eed3e6c0a62dd612
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-25 21:58:32 +0300

    added VisibilityDetectionComponent

---
+++ Assets/Scripts/Components/VisibilityDetectionComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Components
+{
+    public class VisibilityDetectionComponent : MonoBehaviour
+    {
+        public Action<MonoBehaviour> IsVisibleChanged = delegate { };
+
+        private void OnBecameInvisible()
+        {
+            IsVisibleChanged.Invoke(this);
+        }
+
+        private void OnBecameVisible()
+        {
+            IsVisibleChanged.Invoke(this);
+        }
+    }
+}
