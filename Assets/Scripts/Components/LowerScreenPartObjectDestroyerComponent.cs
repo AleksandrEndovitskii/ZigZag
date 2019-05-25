5ddbe66e0fca63214fede1a3c92d73b7c792a7a7
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-26 01:21:19 +0300

    added LowerScreenPartObjectDestroyerComponent

---
+++ Assets/Scripts/Components/LowerScreenPartObjectDestroyerComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class LowerScreenPartObjectDestroyerComponent : MonoBehaviour
+    {
+        private const int SCREEN_PART_DESTROY_TRIGGER = 4; // 1/4 of screen will trigger destruction
+
+        private void Update()
+        {
+            var screenPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+            if (screenPosition.y < Screen.height / SCREEN_PART_DESTROY_TRIGGER)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
