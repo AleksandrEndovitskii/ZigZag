035f12621c1db5debaa6142433fde973f3891c82
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-05-26 03:04:36 +0300

    added crystals counter

---
+++ Assets/Scripts/Components/CrystalsCounterComponent.cs
@@ -0,0 +1,24 @@
+﻿using Managers;
+using TMPro;
+using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class CrystalsCounterComponent : MonoBehaviour
+    {
+        private const string DEFAULT_TEXT = "Collected crystals count: ";
+
+        private void Awake()
+        {
+            CollectedCrystalsCountingServiceCollectedCrystalsCountChanged(GameManager.Instance.CollectedCrystalsCountingService.CollectedCrystalsCount);
+
+            GameManager.Instance.CollectedCrystalsCountingService.CollectedCrystalsCountChanged += CollectedCrystalsCountingServiceCollectedCrystalsCountChanged;
+        }
+
+        private void CollectedCrystalsCountingServiceCollectedCrystalsCountChanged(int collectedCrystalsCount)
+        {
+            this.gameObject.GetComponent<TextMeshProUGUI>().text = DEFAULT_TEXT + collectedCrystalsCount;
+        }
+    }
+}
